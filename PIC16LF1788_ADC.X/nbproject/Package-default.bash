61151abf055a2e3d3ce17119284dee6d0ce9498b
GeorgeCh <georgechirica92@gmail.com>
2020-08-23 16:03:19 +0300

    - renamed project to PIC16LF1788_ADC

--- PIC16LF1788_ADC.X/nbproject/Package-default.bash
+++ PIC16LF1788_ADC.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Base_Config.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Base_Config.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=baseconfig.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/PIC16LF1788_ADC.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=PIC16LF1788_ADC.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=pic16lf1788adc.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/baseconfig.x/bin
+makeDirectory ${TMPDIR}/pic16lf1788adc.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/baseconfig.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/pic16lf1788adc.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/baseconfig.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/pic16lf1788adc.x.tar *
 checkReturnCode
 
 # Cleanup
